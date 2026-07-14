a806ee62e4abe7ae5553a455f07b8392159f9354
Manvesh Bhoyar <manveshbhoyar5@gmail.com>
2026-07-14 23:59:18 +0530

    js conditional statement

---
+++ script.js
@@ -0,0 +1,37 @@
+// // // if statement
+
+// // // let age = 20 ;
+
+// // // if (age >= 18 ) {
+// // //     console.log ("can vote")
+// // // };
+
+
+// // // if else if else
+
+// //    let marks = 75 ;
+
+// //    if (marks >= 60) {
+// //     console.log("C")
+// //    } else if (marks >= 70) {
+// //     console.log("B")
+// //    } else if ( marks >= 80) {
+// //     console.log ("A")
+// //    } else if ( marks >= 90) {
+// //     console.log ("A+")
+// //    } else { 
+// //     console.log("fail");
+// //    }
+
+
+// // Ternary Operator (? :)
+
+// let age = 20;
+
+// let result = (age >= 18) ? "Adult" : "Minor";
+// console.log(result);
+
+let height = 6 ;
+
+let result = (height >= 5.5) ? "Eligible" : "not Eligible";
+console.log(result);
