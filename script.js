a969d71006e7c1b1df785395457c425e509127f6
Manvesh Bhoyar <manveshbhoyar5@gmail.com>
2026-08-24 13:35:12 +0530

    date,time

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-// white space
-const a = "   Hello, World!   ";
+// // white space
+// const a = "   Hello, World!   ";
 
-console.log(a.trim());
+// console.log(a.trim());
 
-const b = "   JavaScript   ";
+// const b = "   JavaScript   ";
 
-console.log(b.trimStart()); 
-console.log(b.trimEnd());  
-console.log(b.trim());      
+// console.log(b.trimStart()); 
+// console.log(b.trimEnd());  
+// console.log(b.trim());      
 
-// spliting string
-const str = "Hello World JavaScript";
+// // spliting string
+// const str = "Hello World JavaScript";
 
-console.log(str.split(" "));
+// console.log(str.split(" "));
+
+// const now = new Date();
+
+// console.log(now);
+
+// date time in js 
+
+//  const now = new Date();
+
+// console.log( now.getDay());
+// console.log("Date:", now.getDate());
+// console.log("Month:", now.getMonth() + 1);
+
+const now = new Date();
+
+console.log(now.toUTCString());
+
